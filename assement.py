232a63fac844c24f840b4d2603c3395ecfa90d48
man-DESTROYER <mandrix.palaone@marcellin.school.nz>
2026-07-28 16:30:17 +1200

    new list widget created with menu widgets

--- assement.py
+++ assement.py
@@ -1,77 +1,77 @@
 import tkinter as tk 
-
+from tkinter import messagebox
 root = tk.Tk()
 root.title("Control interface Alpha")
 root.geometry("500x700")
 
 root.title("Save Entries")
 
+#messagebox.showinfo("information", "what does insert time here mean? \n this button saves your time that you have selected for later.\n When the time you have selected has come it will tell you that your time is soon ariving soon\nWhat is the purpose of haveing this window? \n This window helps users who have less time than others. \n this helps with people rembering things they easliy forget what they are surpost to do")
 
 result_label = tk.Label(root, text="insert time here")
 result_label.pack()
 
-information_label = tk.Label(root, text="what does insert time here mean? \n this button saves your time that you have selected for later.\n When the time you have selected has come it will tell you that your time is soon ariving")
-information_label.pack()
+hour_list =["12", "1", "2" ,"3","4","5","6","7","8","9","10","11"]
+minutes_list = ["00","15", "30","45"]
+am_pm_list = ["AM", "PM"]
 
-information_label = tk.Label(root, text="What is the purpose of haveing this window? \n This window helps users who have less time than others. \n this helps with people rembering things they easliy forget what they are surpost to do")
-information_label.pack()
+hour_value = tk.StringVar(root)
+minutes_value = tk.StringVar(root)
+am_pm_value = tk.StringVar(root)
+
+hour_menu = tk.OptionMenu(root,hour_value, *hour_list)
+hour_menu.pack()
+minutes_menu = tk.OptionMenu(root,minutes_value, *minutes_list)
+minutes_menu.pack()
+am_pm_menu = tk.OptionMenu(root,am_pm_value, *am_pm_list)
+am_pm_menu.pack()
 
 
-go_button = tk.Button(root, text="Go")      # CORRECTcommand=run)add comand later
 
-def greet():
-    name = name_entry.get()                          # read the input
-    result_label.config(text="Kia ora " + name)      # produce output on screen
+
+
+
+
 
 def on_enter(event):                  # bound functions receive an event
     print("Enter was pressed")
 
+
+
 root.title("Listbox demo")
 
-fruit_listbox = tk.Listbox(root)
-fruit_listbox.pack(padx=10, pady=10)
-for fruit in ["Apple", "Banana", "Kiwi"]:
-    fruit_listbox.insert(tk.END, fruit)   # add items at the end
+time_listbox = tk.Listbox(root, width=50)
+time_listbox.pack(padx=10, pady=10)
+
 
 result_label = tk.Label(root, text="")
 result_label.pack()
 
 
-def show_selected():
-    selected = fruit_listbox.curselection()   # tuple of indexes, e.g. (0,)
-    if selected:
-        result_label.config(text="Picked: " + fruit_listbox.get(selected[0]))
+def add_to_list():
+    #selected = fruit_listbox.curselection()   # tuple of indexes, e.g. (0,)
+   
+    if name_time.get() != "":
+        time_listbox.insert(tk.END, name_time.get() + " Due by" + hour_value.get() + minutes_value.get() + am_pm_value.get())   # add items at the end
 
 
-show_button = tk.Button(root, text="Show selected", command=show_selected)
+show_button = tk.Button(root, text="time inserted", command=add_to_list)
 show_button.pack(pady=5)
 
 
 
 
-name_entry = tk.Entry(root)
-name_entry.pack()
-name_entry.bind("<Return>", on_enter)
-greet_button = tk.Button(root, text="Greet", command=greet)
-greet_button.pack()
+name_time = tk.Entry(root)
+name_time.pack()
+name_time.bind("<Return>", on_enter)
+
 result_label = tk.Label(root, text="")
 result_label.pack()
 
-def add_item(item):
-    item_listbox.insert(tk.END, item)
 
 
-apple_button = tk.Button(root, text="Add Apple",
-                         command=lambda: add_item("Apple"))
-apple_button.pack()
-
-def show():
-    # .get() reads the text — always a string
-    result_label.config(text="You typed: " + name_entry.get())
 
 
-show_button = tk.Button(root, text="click me", command=show)
-show_button.pack(pady=5)
 
 root.mainloop()
 
